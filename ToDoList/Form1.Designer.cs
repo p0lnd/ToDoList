846736ed76651762b186cbbbe2660126d0c092a3
p0lnd <polina_usik06@mail.ru>
2025-09-28 18:56:08 +1000

    дизайн

--- ToDoList/Form1.Designer.cs
+++ ToDoList/Form1.Designer.cs
@@ -28,13 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxTasks = new System.Windows.Forms.ListBox();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePickerDueDate = new System.Windows.Forms.DateTimePicker();
+            this.comboBoxCategory = new System.Windows.Forms.ComboBox();
+            this.numericUpDownPriority = new System.Windows.Forms.NumericUpDown();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.checkBoxCompleted = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPriority)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxTasks
+            // 
+            this.listBoxTasks.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listBoxTasks.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.listBoxTasks.FormattingEnabled = true;
+            this.listBoxTasks.ItemHeight = 20;
+            this.listBoxTasks.Location = new System.Drawing.Point(0, 266);
+            this.listBoxTasks.Name = "listBoxTasks";
+            this.listBoxTasks.Size = new System.Drawing.Size(557, 184);
+            this.listBoxTasks.TabIndex = 0;
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBoxTitle.Location = new System.Drawing.Point(115, 25);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(424, 26);
+            this.textBoxTitle.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Задача";
+            // 
+            // dateTimePickerDueDate
+            // 
+            this.dateTimePickerDueDate.Location = new System.Drawing.Point(115, 76);
+            this.dateTimePickerDueDate.Name = "dateTimePickerDueDate";
+            this.dateTimePickerDueDate.Size = new System.Drawing.Size(424, 22);
+            this.dateTimePickerDueDate.TabIndex = 3;
+            // 
+            // comboBoxCategory
+            // 
+            this.comboBoxCategory.FormattingEnabled = true;
+            this.comboBoxCategory.Location = new System.Drawing.Point(115, 124);
+            this.comboBoxCategory.Name = "comboBoxCategory";
+            this.comboBoxCategory.Size = new System.Drawing.Size(424, 24);
+            this.comboBoxCategory.TabIndex = 4;
+            // 
+            // numericUpDownPriority
+            // 
+            this.numericUpDownPriority.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.numericUpDownPriority.Location = new System.Drawing.Point(115, 166);
+            this.numericUpDownPriority.Name = "numericUpDownPriority";
+            this.numericUpDownPriority.Size = new System.Drawing.Size(424, 26);
+            this.numericUpDownPriority.TabIndex = 5;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buttonAdd.Location = new System.Drawing.Point(115, 207);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(109, 43);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.buttonDelete.Location = new System.Drawing.Point(240, 207);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(109, 43);
+            this.buttonDelete.TabIndex = 7;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxCompleted
+            // 
+            this.checkBoxCompleted.AutoSize = true;
+            this.checkBoxCompleted.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.checkBoxCompleted.Location = new System.Drawing.Point(375, 217);
+            this.checkBoxCompleted.Name = "checkBoxCompleted";
+            this.checkBoxCompleted.Size = new System.Drawing.Size(125, 24);
+            this.checkBoxCompleted.TabIndex = 8;
+            this.checkBoxCompleted.Text = "Выполнено";
+            this.checkBoxCompleted.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Срок";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(12, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Категория";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(12, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Приоритет";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(557, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBoxCompleted);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.numericUpDownPriority);
+            this.Controls.Add(this.comboBoxCategory);
+            this.Controls.Add(this.dateTimePickerDueDate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.listBoxTasks);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPriority)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxTasks;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDueDate;
+        private System.Windows.Forms.ComboBox comboBoxCategory;
+        private System.Windows.Forms.NumericUpDown numericUpDownPriority;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.CheckBox checkBoxCompleted;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
